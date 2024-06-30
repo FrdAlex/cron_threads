b1e00c88c3ed40ab31b1fe2f00e891ba8a47c1ec
Alexandru Furdui <alexandru.vlad.furdui@gmail.com>
2024-06-30 12:30:23 +0300

    pylint fixes, add initial files to publish to pypi

---
+++ examples/examples.py
@@ -0,0 +1,12 @@
+from cron_threads import ThreadManager, WorkerModes
+
+def internal(group):
+    """example function to just print whatever is given as argument"""
+    print(group)
+
+manager = ThreadManager()
+manager.begin_manage()
+# Example usage
+manager.start_thread("thread1", "* * * * *",WorkerModes.COMMAND,"dir")
+manager.start_thread("thread2", "* * * * *",WorkerModes.FILE,"file_name",func_name="run")
+manager.start_thread("thread3", None, WorkerModes.INTERNAL, internal, "123")
